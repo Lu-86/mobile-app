f8ba76456d7b8055ae2a3a1cb55b56ab7dc454d8
Lu-86 <gdluciano22@gmail.com>
2026-07-10 23:02:58 -0300

    data added

--- index.js
+++ index.js
@@ -1,3 +1,14 @@
+import { initializeApp } from "https://www.gstatic.com/firebasejs/12.15.0/firebase-app.js";
+
+
+const appSettings = {
+    databaseURL: "https://myapp-62c90-default-rtdb.firebaseio.com/"
+}
+
+const app = initializeApp(appSettings);
+
+console.log(app);
+
 const button = document.getElementById("add-button");
 const itemValue = document.getElementById("input-field");
 
