d6f8b55b1006020dbca6f52d9f5322a41051c756
Lu-86 <gdluciano22@gmail.com>
2026-07-13 19:04:34 -0300

    database added

--- index.js
+++ index.js
@@ -1,18 +1,27 @@
 import { initializeApp } from "https://www.gstatic.com/firebasejs/12.15.0/firebase-app.js";
-
+import { getDatabase, ref, push, onValue } from "https://www.gstatic.com/firebasejs/12.15.0/firebase-database.js";
 
 const appSettings = {
     databaseURL: "https://myapp-62c90-default-rtdb.firebaseio.com/"
 }
 
 const app = initializeApp(appSettings);
+const database = getDatabase(app);
+const moviesInDB = ref(database, "movies")
 
-console.log(app);
-
-const button = document.getElementById("add-button");
 const itemValue = document.getElementById("input-field");
+const button = document.getElementById("add-button");
 
 button.addEventListener("click", function () {
-    let item = itemValue.value;
-    console.log(item);
+    let item = itemValue.value.trim();
+
+    if (item !== "") {
+        push(moviesInDB, item);
+        itemValue.value = "";
+    }
+});
+
+onValue(moviesInDB, function (snapshot) {
+    const data = (snapshot.val());
+    console.log(data);
 });
